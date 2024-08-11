3bfa700767351559b3ace99fd4c29e08359a774f
sam owen <36443549+StructuredTrading@users.noreply.github.com>
2024-08-11 22:02:32 +1000

    Delete client - Copy directory

--- client - Copy/src/index.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles/style.scss';
-
-createRoot(document.getElementById('root')).render(<App />);
